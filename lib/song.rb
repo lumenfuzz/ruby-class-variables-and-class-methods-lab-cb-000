f4854a948c03a68d419dd1a98ab2e32e155aa89a
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-22 00:42:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,33 @@
 class Song
 
   attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
 
   def initialize(title, artist, genre)
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
     @name = title
+    @artist = artist
+    @genre = genre
   end
-  
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    return @@artists.uniq
+  end
+
+  def self.genres
+    return @@genres.uniq
+  end
+
+  def self.genre_count
+  end
+
+  def self.artist_count
+  end
 end
