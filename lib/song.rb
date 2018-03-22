e76a4b9e78cfc96bc701ef7e600be3e07787ddee
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-22 00:56:28 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(title, artist, genre)
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+    @name = title
+    @artist = artist
+    @genre = genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    return @@artists.uniq
+  end
+
+  def self.genres
+    return @@genres.uniq
+  end
+
+  def self.genre_count
+    the_count_hash = {}
+    @@genres.each do |genre|
+      if the_count_hash.key?(genre)
+        the_count_hash[genre] += 1
+      else
+        the_count_hash.store(genre, 1)
+      end
+    end
+    return the_count_hash
+  end
+
+  def self.artist_count
+    the_count_hash = {}
+    @@artists.each do |artist|
+      if the_count_hash.key?(artist)
+        the_count_hash[artist] += 1
+      else
+        the_count_hash.store(artist, 1)
+      end
+    end
+    return the_count_hash
+  end
+end
