22e18890311dfb22df5daf7b52f3e1957b35f168
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-22 00:55:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,8 +26,26 @@ class Song
   end
 
   def self.genre_count
+    the_count_hash = {}
+    @@genres.each do |genre|
+      if the_count_hash.key?(genre)
+        the_count_hash[genre] += 1
+      else
+        the_count_hash.store(genre, 1)
+      end
+    end
+    return the_count_hash
   end
 
   def self.artist_count
+    the_count_hash = {}
+    @@artists.each do |artist|
+      if the_count_hash.key?(artist)
+        the_count_hash[artist] += 1
+      else
+        the_count_hash.store(artist, 1)
+      end
+    end
+    return the_count_hash
   end
 end
